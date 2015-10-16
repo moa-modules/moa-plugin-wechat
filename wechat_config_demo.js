cfef44b1f041cf8e7f84746a6194f93337848987
i5ting <i5ting@126.com>
2015-10-16 12:17:11 +0800

    init weichat_config_demo

---
+++ wechat_config_demo.js
@@ -0,0 +1,28 @@
+/*!
+ * Moajs Middle
+ * Copyright(c) 2015-2019 Alfred Sang <shiren1118@126.com>
+ * MIT Licensed
+ */
+var fs = require('fs');
+
+
+// for raw data post
+module.exports = function (req, res, next) {
+  req.wx = {
+    'app_id' : '',
+    'app_secret' : '',
+    'domain':'https://xxxxx.localtunnel.me',
+    'app_token':'xxxx.com',
+    'enable_admin': true,
+    //for pay
+    'mch_id': 'xxxxx',
+    'pfx': fs.readFileSync('./pay/cert/apiclient_cert.p12'), //微信商户平台证书
+    //callback
+    callback:{
+      success : '/wechats',
+      failed  : '/404'
+    }
+  }
+  
+  next();
+};
